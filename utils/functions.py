c35e15e6940dd43f709252dfc9d30f36f0d48fbf
ambra-dipiano <ambra.dipiano@inaf.it>
2022-09-19 18:05:15 +0200

    add get_target_coords()

--- utils/functions.py
+++ utils/functions.py
@@ -5,6 +5,8 @@
 # This software contains an ensable of utility functions
 # *****************************************************************************
 
+import xml.etree.ElementTree as ET
+
 def mjd_to_met(time):
     """Convert mean julian date to mission elapse time."""
     correction = 0.0006962818548948615
@@ -14,3 +16,9 @@ def met_to_mjd(time):
     """Convert mean julian date to mission elapse time."""
     correction = 0.0006962818548948615
     return time / (86400. + correction) + 51910
+
+def get_target_coords(model, name):
+    mysource = ET.parse(model).getroot().find('source[@name="' + name + '"]')
+    ra = mysource.find('spatialModel/parameter[@name=RA]').get('value')
+    dec = mysource.find('spatialModel/parameter[@name=DEC]').get('value')
+    return ra, dec
