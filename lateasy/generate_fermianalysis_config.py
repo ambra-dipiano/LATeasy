e689246cd0ae043bde288c2f3aa6634ceaa5bc72
ambra-dipiano <ambra.dipiano@inaf.it>
2022-09-22 12:01:58 +0200

    fix abspath, bash script write and slurm submission option

--- lateasy/generate_fermianalysis_config.py
+++ lateasy/generate_fermianalysis_config.py
@@ -7,7 +7,7 @@
 # *****************************************************************************
 
 
-import sys
+import lateasy
 import argparse
 import yaml
 import pandas as pd
@@ -32,71 +32,76 @@ log = set_logger(filename=logname, level=pipeconf['execute']['loglevel'])
 
 # generate all execution scripts and configuration
 def generate(name, tmin, tmax, emax, queue, data):
+    # define file names
+    dirname = join(pipeconf['path']['output'], name+"_"+str(tmin)+"_"+str(tmax))
+    ymlname = join(pipeconf['path']['output'], name+"_"+str(tmin)+"_"+str(tmax)+".yml")
+    llname = join(pipeconf['path']['output'], name+"_"+str(tmin)+"_"+str(tmax)+".ll")
+    shname = join(pipeconf['path']['output'], name+"_"+str(tmin)+"_"+str(tmax)+".sh")
+    
     # complete fermipy configuration
     fermiconf['selection']['emax'] = emax
     fermiconf['selection']['tmin'] = tmin
     fermiconf['selection']['tmax'] = tmax
-    fermiconf['fileio']['outdir'] = str(name) + "_" + str(tmin) + "_" + str(tmax)
+    fermiconf['fileio']['outdir'] = dirname
 
-    # define file names
-    dirname = name+"_"+str(tmin)+"_"+str(tmax)
-    ymlname = name+"_"+str(tmin)+"_"+str(tmax)+".yml"
-    llname = name+"_"+str(tmin)+"_"+str(tmax)+".ll"
-    shname = name+"_"+str(tmin)+"_"+str(tmax)+".sh"
-    
     # write fermipy yaml configuration
     with open(ymlname, 'w') as f:
         yaml.dump(fermiconf, f)
-    
-    if pipeconf['background']['isonorm'] is None:
-        pipeconf['background']['isonorm'] = 1
-    if pipeconf['background']['galnorm'] is None:
-        pipeconf['background']['galnorm'] = 1
-    if pipeconf['background']['galindex'] is None:
-        pipeconf['background']['galindex'] = 0
 
+    # define background values
     if not data.empty:
+        # from previous results
         for index, row in data.iterrows():
             if tmin >= row["tmin"] and tmin <= row["tmax"]:
                 pipeconf['background']['isonorm'] = row["iso_P8R3_SOURCE_V2_v1_Normalization_value"]
                 pipeconf['background']['galnorm'] =  row["gll_iem_v07_Prefactor_value"]
                 pipeconf['background']['galindex'] = row["gll_iem_v07_Index_value"]
+                log.info('Background from precomputed lightcurve results')
+    elif all([pipeconf['background']['isonorm'], pipeconf['background']['galnorm'], pipeconf['background']['galindex']]) is not None:
+        # from configuration
+        log.info('Background from configuration file')
+    else:
+        # from default values
+        pipeconf['background']['isonorm'] = 1
+        pipeconf['background']['galnorm'] = 1
+        pipeconf['background']['galindex'] = 0
+        log.info('Background from default values')
+
+    # compose bash executable script
+    job = [
+    '#!/bin/bash\n\n',
+    'source activate ' + pipeconf['slurm']['envname'],
+    '\nexport FERMIDATA=' + pipeconf['path']['data'],
+    '\nexport FERMI_DIFFUSE_DIR=' + pipeconf['path']['galdir'],
+    '\npython ' + join(abspath(lateasy.__file__), 'run_fermianalysis.py') + ' --fermiconf ' + ymlname + ' --pipeconf ' + args.pipeconf, 
+    '\ncp ' + dirname + '.* ' + dirname,
+    '\ncd ' + dirname,
+    '\npython ' + join(abspath(lateasy.__file__) + 'run_sensitivity.py'),
+    '\nmv ' + join(dirname, 'ltcube_00.fits') + ' ' + join(dirname, 'ltcube_00.fits.tmp'),
+    '\nrm ' + join(dirname, '*.fits'),
+    '\nmv ' + join(dirname, 'ltcube_00.fits.tmp') + ' ' + join(dirname, 'ltcube_00.fits'),
+    '\nrm ' + join(dirname, '*.par'),
+    ]
 
     # write bash executable script
-    job = f"""source activate {pipeconf['slurm']['envname']}
-    export FERMIDATA={pipeconf['path']['data']}
-    export FERMI_DIFFUSE_DIR={pipeconf['path']['galdir']}
-    python {abspath(__file__)}/run_fermianalysis.py --fermiconf {ymlname} --pipeconf {args.pipeconf}
-    cp {dirname}.* {dirname}
-    cd {dirname}
-    python {abspath(__file__)}/run_fermipy_sensitivity.py
-    mv ltcube_00.fits ltcube_00.fits.tmp
-    rm *.fits
-    mv ltcube_00.fits.tmp ltcube_00.fits
-    rm *.par
-    cd .."""
-     
     with open(shname, "w") as sh:
-        sh.write(job)
+        sh.writelines(job)
 
     # load slurm job submission template
     with open(pipeconf['slurm']['template']) as ll:
         job = ll.read()
 
-    # replace executable bash filename to launch
-    job.replace('$BASH_NAME$', shname)
+    # replace executable bash filename 
+    job = job.replace('$BASH_NAME$', shname)
+
+    # write job submission script
     with open(llname, "w") as ll:
         ll.write(job)
 
-    system("sbatch --partition="+queue+" " + llname)
+    # submit job to slurm
+    if pipeconf['slurm']['sbatch']:
+        system("sbatch --partition=" + queue + " " + llname)
 
-name = pipeconf['slurm']['name']
-tmin =  pipeconf['slurm']['tmin']
-tmax =  pipeconf['slurm']['tmax']
-mode =  pipeconf['slurm']['mode']
-timebinsize = pipeconf['slurm']['timebinsize']
-emax = pipeconf['slurm']['emax']
-queue = pipeconf['slurm']['queue']
 
 # file with result to extract isomodel and galmodel parameters (if absent take then compute from default)
 if pipeconf['slurm']['bkgresults'] is not None and isfile(join(pipeconf['path']['output'], pipeconf['slurm']['bkgresults'])):
@@ -106,16 +111,29 @@ else:
     column_names = ["a", "b", "c"]
     data = pd.DataFrame(columns = column_names)
 
-if mode == 0:
+# assign variables
+name = pipeconf['slurm']['name']
+tmin =  pipeconf['slurm']['tmin']
+tmax =  pipeconf['slurm']['tmax']
+mode =  pipeconf['slurm']['mode']
+binsize = pipeconf['lightcurve']['binsize']
+emax = pipeconf['slurm']['emax']
+queue = pipeconf['slurm']['queue']
+
+# submitt jobs based on "mode"
+if mode.lower() == 'hours':
     # compute at hours timescale from 1 day prior tmin to 1 day after tmax
     for i in range(tmin-86400, tmax+86400, 3600):
-        generate(name, i, i+timebinsize, emax, queue, data)
-if mode == 1:
+        generate(name, i, i+binsize, emax, queue, data)
+elif mode.lower() == 'fix':
     # compute at given timescale from tmin to tmax
-    for i in range(tmin, tmax, timebinsize):
-        generate(name, i, i+timebinsize, emax, queue, data)
-if mode == 2:
+    for i in range(tmin, tmax, binsize):
+        generate(name, i, i+binsize, emax, queue, data)
+elif mode.lower() == 'integral':
     # compute integral from tmin to tmax
     generate(name, tmin, tmax, emax, queue, data)
+else:
+    # invalid "mode" configuration
+    raise ValueError('Invalid "mode" configuration:', mode.lower())
 
 
