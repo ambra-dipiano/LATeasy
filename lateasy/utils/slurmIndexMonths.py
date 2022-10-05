59eb73916817388d011ec24643fdc05892dbd682
ambra-dipiano <ambra.dipiano@inaf.it>
2022-10-05 13:26:09 +0200

    refactoring with conf

--- lateasy/utils/slurmIndexMonths.py
+++ lateasy/utils/slurmIndexMonths.py
@@ -1,13 +1,32 @@
-import sys
-from os import listdir, system
-from os.path import isfile, join
+# *****************************************************************************
+# Copyright (C) 2022 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# This software is intended to rename slurm outputs as directory logging
+# *****************************************************************************
 
-if len(sys.argv) > 1:
-    folder = sys.argv[1]
-else:
-    folder = 'YEARS5h'
-path = '/data01/projects/IGRJ17354-3255/FERMI/LC/' + folder
-logs = [f for f in listdir(path) if 'slurm' in f and 'index' not in f and 'M' not in f and isfile(join(path, f))]
+import yaml
+import argparse
+from os import listdir, system
+from os.path import isfile, join, basename
+from lateasy.utils.functions import set_logger
+
+parser = argparse.ArgumentParser(description='Fermi/LAT data analysis pipeline')
+parser.add_argument('--pipeconf',  type=str, required=True, help='configuration file')
+args = parser.parse_args()
+
+# load yaml configurations
+with open(args.pipeconf) as f:
+    pipeconf = yaml.safe_load(f)
+
+# logging
+logname = join(pipeconf['path']['output'], basename(__file__).replace('.py','.log'))
+log = set_logger(filename=logname, level=pipeconf['execute']['loglevel'])
+log.info('Logging: ' + logname)
+
+
+path = pipeconf['path']['output']
+logs = [f for f in listdir(path) if 'slurm' in f and 'index' not in f and pipeconf['slurm']['name'] not in f and isfile(join(path, f))]
 
 # write index
 indexfile = join(path, 'slurm-index.txt')
@@ -35,7 +54,7 @@ for i, f in enumerate(logs):
 
 logs = [f for f in listdir(path) if 'slurm' in f and 'index' not in f and 'M' in f and isfile(join(path, f))]
 # write index
-indexfile = join(path.replace(folder, ''), 'igrMonthsIndex.out')
+indexfile = join(path, 'igrMonthsIndex.out')
 print(indexfile)
 hdr = 'slurm-id.out month tstart tstop\n'
 with open(indexfile, 'w+') as f:
