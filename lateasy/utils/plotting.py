e888268e85aced180fe5f23d10c1f0d6c380dd2c
ambra-dipiano <ambra.dipiano@inaf.it>
2022-10-14 13:02:38 +0200

    fix MET - MJD conversion

--- lateasy/utils/plotting.py
+++ lateasy/utils/plotting.py
@@ -10,7 +10,8 @@ import numpy as np
 import matplotlib
 import pandas as pd
 from os.path import join, basename
-from lateasy.utils.functions import set_logger
+from lateasy.utils.functions import set_logger, met_to_mjd
+from astropy.io import fits
 
 class Plotting():
     def __init__(self, pipeconf):
@@ -34,7 +35,7 @@ class Plotting():
         data = pd.read_csv(filename, sep=" ", header=0)
         return data
 
-    def plot_lc(self, data, filename, fontsize=20):
+    def plot_lc(self, data, filename='lightcurve.png', fontsize=20):
         # get data
         ts = np.array(data['ts'])
         t = (((np.array(data['tmin_mjd']) + np.array(data['tmax_mjd'])) / 2)) 
@@ -75,5 +76,75 @@ class Plotting():
         matplotlib.pyplot.close()
         self.log.info('plotting' + filename)
         return self
+
+    def compare_lc_full_range(self, data_lc, data_fermi, filename='lightcurve_comparison.png', fontsize=15):
+
+        # get data_lc
+        ts = np.array(data_lc['ts'])
+        t = (np.array(data_lc['tmin_mjd']) + np.array(data_lc['tmax_mjd'])) / 2
+        terr = (np.array(data_lc['tmax_mjd']) - np.array(data_lc['tmin_mjd'])) / 2
+        f = np.array(data_lc['flux'])
+        ferr = np.array(data_lc['flux_err'])
+
+        # get upper limits in data_lc
+        upl = []
+        for idx, (v, e, tv) in enumerate(zip(f, ferr, ts)):
+            if e > 2*v or tv < self.pipeconf['postprocessing']['mints']:
+                upl.append(True)
+                ferr[idx] = v/2
+            else:
+                upl.append(False)
+        
+        # get detections in data_lc
+        detection = [f[i] for i in range(len(f)) if not upl[i]]
+        detection_err = [ferr[i] for i in range(len(ferr)) if not upl[i]]
+        detection_time = [t[i] for i in range(len(t)) if not upl[i]]
+        detection_ts = [ts[i] for i in range(len(ts)) if not upl[i]]
+        self.log.debug('Number of detection above TS=' + str(self.pipeconf['postprocessing']['mints']) + ' : ' + str(len(detection)))
+
+        # filter fermi data
+        self.log.debug('Min LAT time: ' + str(min(met_to_mjd(data_fermi['START']))))
+        self.log.debug('Min DATA time: ' + str(min(t)))
+        self.log.info('Filter LAT data for time > ' + str(min(t)))
+        self.log.debug('Max LAT time: ' + str(max(met_to_mjd(data_fermi['STOP']))))
+        self.log.debug('Max DATA time: ' + str(max(t)))
+        self.log.info('Filter LAT data for time < ' + str(max(t)))
+        data_fermi['START'] = met_to_mjd(data_fermi['START'])
+        data_fermi['STOP'] = met_to_mjd(data_fermi['STOP'])
+        data_fermi = data_fermi[((data_fermi['START']) >= np.min(t)) & (data_fermi['STOP'] <= np.max(t))]
+        self.log.info('Length of intersection:' + str(len(data_fermi)))
+
+        # get data_fermi
+        fermi_time = (np.array(data_fermi['START']) + np.array(data_fermi['STOP'])) / 2
+        fermi_time_err = np.array(data_fermi['STOP'] - np.array(data_fermi['START'])) / 2
+        fermi_flux = np.array(data_fermi['FLUX_100_300000'])
+        fermi_flux_err = np.array(data_fermi['ERROR_100_300000'])
+        fermi_upl = np.array(data_fermi['UL_100_300000'])
+        fermi_ts = np.array(data_fermi['TEST_STATISTIC'])
+
+        # plot flux
+        fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1, sharex=True, figsize=(10,8))
+        ax1.set_title(self.pipeconf['target']['name'] + ' lightcurve', fontsize=fontsize)
+        ax1.errorbar(t, f, xerr=terr, yerr=ferr, ls=' ', marker='o', markeredgecolor='k', uplims=upl, color='b', zorder=0, label='Fermi/LAT local analysis')
+        ax1.errorbar(fermi_time, fermi_flux, xerr=fermi_time_err, yerr=fermi_flux_err, ls=' ', marker='P', markeredgecolor='k', uplims=fermi_upl, color='g', zorder=5, label='Fermi/LAT data repository')
+        ax1.set_ylabel('flux (ph/cm2/s)', fontsize=fontsize)
+        ax1.set_yscale('log')
+        # plot ts
+        ax2.errorbar(t, ts, xerr=terr, ls=' ', marker='o', markeredgecolor='k', color='b', label='Fermi/LAT local analysis')
+        ax2.errorbar(fermi_time, fermi_ts, xerr=fermi_time_err, ls=' ', marker='P', markeredgecolor='k', color='g', label='Fermi/LAT data repository')
+        ax2.axhline(self.pipeconf['postprocessing']['mints'], ls='-.', color='r')
+        ax2.set_xlabel('time (MJD)', fontsize=fontsize)
+        ax2.set_ylabel('TS', fontsize=fontsize)
+        matplotlib.pyplot.legend(loc=0, fontsize=fontsize, bbox_to_anchor=(0.4, 0.9))
+        matplotlib.pyplot.tight_layout()
+        fig.savefig(filename)
+        matplotlib.pyplot.close()
+        self.log.info('plotting' + filename)
+        return self
+
+    def load_data_lc_from_fits(self, filename):
+        with fits.open(filename) as h:
+            lc = h['LIGHTCURVES'].data
+        return lc
 
 
